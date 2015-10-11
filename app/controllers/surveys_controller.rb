2d29c08e7d73470a92f2ba587c19a8db811e166f
David Bernheisel <david@bernheisel.com>
2015-10-11 15:22:07 -0400

    Fix views

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -60,6 +60,6 @@ class SurveysController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def survey_params
-      params.require(:survey).permit(:title, :question_id, :type, :radio, :short, :question_starter)
+      params.require(:survey).permit(:title, :question_id, :type, :radio, :short, :user_id)
     end
 end
